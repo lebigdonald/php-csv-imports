9c0a8a67ec9918679a82763153bb22778d3d9ba5
Donald NGUIMFACK <nkengfack96@gmail.com>
2024-08-05 08:50:58 +0100

    SMS Update - Update Member

--- sms-update.php
+++ sms-update.php
@@ -1,7 +1,7 @@
 <?php
-if (isset($_POST["sms_list"])) {
-    $filename = $_FILES['file']['tmp_name'];
-    if ($_FILES['file']['size'] > 0) {
+if (isset($_POST["sms_list_status"])) {
+    $filename = $_FILES['file_status']['tmp_name'];
+    if ($_FILES['file_status']['size'] > 0) {
         $file = fopen($filename, "r");
         $line = 0;
         $result = [];
@@ -26,7 +26,54 @@ if (isset($_POST["sms_list"])) {
             $query .= ($i < count($messages) - 1) ? "message = '" . $message . "' OR \n" : "message = '" . $message . "');";
         }
 
-        $filename = 'output.txt';
+        $filename = 'sms_status_output.txt';
+
+        if (file_put_contents($filename, $query)) {
+            echo "<hr /><p><h1>$line  SMS Query Generated</h1> <br /></p>";
+        } else {
+            echo "<hr /><p><h1>$line  SMS Query Not Generated</h1> <br /></p>";
+        }
+
+        fclose($file);
+    } else {
+        echo "Please choose a file";
+        exit();
+    }
+}
+
+if (isset($_POST["sms_list_member"])) {
+    $filename = $_FILES['file_member']['tmp_name'];
+    if ($_FILES['file_member']['size'] > 0) {
+        $file = fopen($filename, "r");
+        $line = 0;
+        $result = [];
+
+        $messages = [];
+        fgetcsv($file);
+
+        while (($getData = fgetcsv($file, 0, ","))) {
+            try {
+                if ($getData[0] !== '') {
+                    $messages[] = [
+                        'idcollect_mem' => $getData[0],
+                        'phone1' => $getData[7]
+                    ];
+                    $line++;
+                }
+            } catch (Exception $ex) {
+                die($ex);
+            }
+        }
+
+        $query = "";
+        for ($i = 0; $i < count($messages); $i++) {
+            $message = $messages[$i];
+            if ($message['phone1'] !== '') {
+                $query .= "UPDATE omegamfi_oc_classic.sms SET member = {$message['idcollect_mem']} WHERE receiver = '237{$message['phone1']}';\n";
+            }
+        }
+
+        $filename = 'sms_member_output.txt';
 
         if (file_put_contents($filename, $query)) {
             echo "<hr /><p><h1>$line  SMS Query Generated</h1> <br /></p>";
@@ -52,8 +99,15 @@ if (isset($_POST["sms_list"])) {
 <div class="login-page">
     <div class="form">
         <form class="login-form" method="POST" action="" enctype="multipart/form-data">
-            Fichier des SMS : <input type="file" name="file" id="file" required/> <br>
-            <input type="submit" name="sms_list" class="button" value="Generate Query">
+            Fichier des SMS (Status) : <input type="file" name="file_status" id="file_status" required/> <br>
+            <input type="submit" name="sms_list_status" class="button" value="Generate Query">
+        </form>
+    </div>
+
+    <div class="form">
+        <form class="login-form" method="POST" action="" enctype="multipart/form-data">
+            Fichier des SMS (Member) : <input type="file" name="file_member" id="file_member" required/> <br>
+            <input type="submit" name="sms_list_member" class="button" value="Generate Query">
         </form>
     </div>
 </div>
